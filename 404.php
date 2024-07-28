0812d45fa28bb46dc03c34b8d221cb61b2296597
start-mage <support@startmage.io>
2024-07-29 01:41:41 +0200

    add 404 php

--- 404.php
+++ 404.php
@@ -1,6 +1,12 @@
 <?php
-
-http_response_code(200);
-include 'index.html';
+$url = $_SERVER['REQUEST_URI'];
+echo $url;
+if(file_exists($url)) {
+    http_response_code(200);
+    include 'index.html';
+}else{
+    http_response_code(404);
+    die('404');
+}
 
 ?>
